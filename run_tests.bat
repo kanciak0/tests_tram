6c47ca2893898527cfc9e6e54257c152cce85bd8
mateusz.mocko <mateusz.mocko@dgt.eu>
2024-09-16 09:01:00 +0200

    Chore:Lint

--- run_tests.bat
+++ run_tests.bat
@@ -4,7 +4,7 @@ REM Navigate to the target directory
 cd /d "C:\Users\dgtla\PycharmProjects\Tram_testy\tram_testy\tests\"
 
 REM Run pytest with the first config file and log everything
-start "" cmd /c pytest --serial-config=../common/config_file.txt --tb=short --log-cli-level=DEBUG --log-cli-date-format= --log-file=pytest_output_1.log --reruns 1
+start "" cmd /c pytest --serial-config=../common/config_file.txt --tb=short --log-cli-level=DEBUG --log-cli-date-format= --log-file=pytest_output_1.log
 
 
 REM Run pytest with the second config file and log everything
